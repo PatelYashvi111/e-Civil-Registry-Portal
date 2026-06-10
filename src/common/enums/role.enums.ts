ddda4534991c1e51b253fceee35d19612ba5b779
PatelYashvi <yashvipatel235@gmail.com>
2026-06-09 19:30:56 -0700

    add role schema

---
+++ src/common/enums/role.enums.ts
@@ -0,0 +1,5 @@
+export enum RoleEnum {
+  ADMIN = 'Admin',
+  CLERK = 'Clerk',
+  USER = 'User',
+}
---
+++ src/role/schema/role.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { RoleEnum } from '../../common/enums/role.enums';
+
+export type RoleDocument = HydratedDocument<Role>;
+
+@Schema({
+  timestamps: true,
+})
+export class Role {
+  @Prop({
+    required: true,
+    unique: true,
+    enum: RoleEnum,
+  })
+  name!: RoleEnum;
+}
+
+export const RoleSchema = SchemaFactory.createForClass(Role);
