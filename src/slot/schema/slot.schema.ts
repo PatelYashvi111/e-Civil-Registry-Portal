af01b4b63e844e8cf04e231f733fe58d4d9e0ce4
PatelYashvi <yashvipatel235@gmail.com>
2026-06-23 15:55:32 -0700

    feat(service):save changes

--- src/slot/schema/slot.schema.ts
+++ src/slot/schema/slot.schema.ts
@@ -18,35 +18,41 @@ export class Slot {
 
     @Prop({
         required: true,
+        type: Date,
     })
     slotDate!: Date;
 
     @Prop({
         required: true,
         trim: true,
+        type: String,
     })
     startTime!: string;
 
     @Prop({
         required: true,
         trim: true,
+        type: String,
     })
     endTime!: string;
     
     @Prop({
         required: true,
         min: 1,
+        type: Number,
     })
     maxCapacity!: number;
 
     @Prop({
         default: 0,
         min: 0,
+        type: Number,
     })
     bookedCount!: number;
 
     @Prop({
         default: true,
+        type: Boolean,
     })
     isAvailable!: boolean;
 
